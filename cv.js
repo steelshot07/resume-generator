d1c1d9a9cd8a561cf617d5e9a37ce3fc68e3234d
steelshot07 <67476656+steelshot07@users.noreply.github.com>
2025-02-18 19:05:05 +0530

    Update cv.js

--- cv.js
+++ cv.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("cv-work-experience").innerHTML = localStorage.getItem("work_experience") || "Not provided";
     document.getElementById("cv-academic-qualification").innerHTML = localStorage.getItem("academic_qualification") || "Not provided";
 
-    document.getElementById("cv-name").innerText = localStorage.getItem("head_name") || "Not provided";
+    document.getElementById("cv-name").innerText = localStorage.getItem("cv_name") || "Not provided";
 
     });
 
